1a53a8b673280562d2600059f9f9dc0cff59753d
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-06-27 22:50:05 -0400

    Updates

---
+++ precache-manifest.4be7727760486b1401c7158dea7db417.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7ae0ec2e48abc7625450b13313415d",
+    "url": "/JRan-37/index.html"
+  },
+  {
+    "revision": "44cbbf3d3a833306f9b8",
+    "url": "/JRan-37/static/css/main.ec160b96.chunk.css"
+  },
+  {
+    "revision": "fb9a45f51f108f41e158",
+    "url": "/JRan-37/static/js/2.3fc1edd8.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/JRan-37/static/js/2.3fc1edd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cbbf3d3a833306f9b8",
+    "url": "/JRan-37/static/js/main.9e42f6a2.chunk.js"
+  },
+  {
+    "revision": "e95ddd723014f2336a61",
+    "url": "/JRan-37/static/js/runtime-main.7fcf7134.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/JRan-37/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "730b3cf4ce8d8cfb54a2f24e4a453fa3",
+    "url": "/JRan-37/static/media/At-PCC-Logo.730b3cf4.svg"
+  },
+  {
+    "revision": "406f7a2260c7a125b2eee550f8956f23",
+    "url": "/JRan-37/static/media/CIPPL.406f7a22.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/JRan-37/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/JRan-37/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/JRan-37/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/JRan-37/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/JRan-37/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/JRan-37/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/JRan-37/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f339da8a3c67bb5ad14b30cb3f0410ea",
+    "url": "/JRan-37/static/media/SSLLogo.f339da8a.png"
+  },
+  {
+    "revision": "e6658e80d6b950ab9191eee7fe1696d0",
+    "url": "/JRan-37/static/media/University_of_Pittsburgh_seal.e6658e80.png"
+  },
+  {
+    "revision": "fd528e6a753567c2bb14f9c9b7b8b297",
+    "url": "/JRan-37/static/media/University_of_Pittsburgh_seal.fd528e6a.svg"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/JRan-37/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/JRan-37/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/JRan-37/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/JRan-37/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/JRan-37/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/JRan-37/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/JRan-37/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/JRan-37/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/JRan-37/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/JRan-37/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/JRan-37/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/JRan-37/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/JRan-37/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/JRan-37/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/JRan-37/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/JRan-37/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/JRan-37/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/JRan-37/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/JRan-37/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "f7288055334eeee2693e025ee2a8a8ee",
+    "url": "/JRan-37/static/media/hackathon1.f7288055.svg"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "/JRan-37/static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "47336f09289442ec77717c1405628901",
+    "url": "/JRan-37/static/media/jrprofile.47336f09.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/JRan-37/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/JRan-37/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "3d7f6192b4267dcae8d0f72b4b9e24a5",
+    "url": "/JRan-37/static/media/nextgen-logo.3d7f6192.svg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/JRan-37/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/JRan-37/static/media/postman.c108f557.png"
+  }
+]);
